49f96c2faedfb95194d52a9aa549476da8ed7eca
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-05-25 01:25:39 -0400

    Full functionality of game from command line

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,6 +9,9 @@ class Game
   			[1, 4, 7], [2, 5, 8], [3, 6, 9],
   			[1, 5, 9], [3, 5, 7]
 
+    @selected_numbers = []
+  	@counter = 0
+
   	game_board
   	start_player1
   end
@@ -27,13 +30,41 @@ class Game
   def start_player1
   	puts "#{@player1}: Please enter a number for your X to go"
   	player1_input = gets.chomp
+    validate_input(player1_input)
+    @selected_numbers << player1_input
+
   	locate_player1_input(player1_input)
   end
   
   def start_player2	
   	puts "#{@player2}: Please enter a number for your O to go"
   	player2_input = gets.chomp
+    validate_input2(player2_input)
+    @selected_numbers << player2_input
+    
   	locate_player2_input(player2_input)		
+  end
+
+  def valid_input?(num)
+    return true if num.match(/^\d$/)
+
+    false
+  end
+
+  def validate_input(num)
+    if @selected_numbers.include?(num)
+      puts "That number has already been chosen, please select again"
+      game_board
+      start_player1
+    end
+  end
+
+  def validate_input2(num)
+    if @selected_numbers.include?(num)
+      puts "That number has already been chosen, please select again"
+      game_board
+      start_player2
+    end
   end
 
   def locate_player1_input(num)
@@ -46,12 +77,15 @@ class Game
   		end
   	else
   		puts "Please select a digit between 1 and 9"
+      game_board
   		start_player1
   	end
 
   	if win? == true
   		puts "Congratulations #{@player1}, you got 3 in a row!"
+      play_again
   	else
+  		tie_game?
   		start_player2
   	end	
   end
@@ -63,15 +97,18 @@ class Game
 
   		@rows.each do |r|
   	  		puts r.each { |c| c }.join(" ")
-  		end
+  		end	
   	else
   		puts "Please select a digit between 1 and 9"
+      game_board
   		start_player2
   	end	
 
   	if win2? == true
   		puts "Congratulations #{@player2}, you got 3 in a row!"
+      play_again
   	else
+  		tie_game?
   		start_player1
   	end	 	
   end
@@ -92,10 +129,29 @@ class Game
   	false
   end
 
-  def valid_input?(num)
-  	return true if num.match(/^\d$/)
+  def tie_game?
+  	@counter += 1
+  	if @counter == 9 
+  		puts "The game was a tie. Nicely played #{@player1} & #{@player2}!"
+      play_again  
+  	end
 
-  	false
+  	return true
+  end
+
+  def play_again
+    puts "Would you like to play again? (Y / N)"
+    user_response = gets.chomp.downcase
+
+      if user_response == "y"
+        Game.new("Nick", "Harry")
+      elsif user_response == "n"
+        puts "Okay, thanks for playing!"
+        exit
+      else
+        puts "Please enter 'Y' or 'N'"
+        play_again
+      end
   end
 end
 
